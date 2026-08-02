787576a3fbbd7857c627c12d23d46f3691602bb3
Muhammad Ali <105352723+muhammmadalli@users.noreply.github.com>
2026-08-03 00:49:21 +0500

    Configure QuestPDF license on app startup
    Adds QuestPDF import and sets QuestPDF.Settings.License = LicenseType.Community in App.xaml.cs constructor. Ensures the QuestPDF library is configured with the Community license at application startup to avoid license-related runtime issues.

--- ASTquizApp/App.xaml.cs
+++ ASTquizApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using QuestPDF.Infrastructure;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,11 @@ namespace ASTquizApp
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            QuestPDF.Settings.License =
+                LicenseType.Community;
+        }
     }
 
 }
