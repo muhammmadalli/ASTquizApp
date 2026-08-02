8b74469ceb7c673e34b577f5b6797d8605ba7df2
Muhammad Ali <105352723+muhammmadalli@users.noreply.github.com>
2026-08-03 00:21:41 +0500

    Add Result model
    Introduce Result model (ASTquizApp.Models.Result) to represent quiz outcomes with CandidateName, TotalQuestions, CorrectAnswers, WrongAnswers, and Percentage properties.

---
+++ ASTquizApp/Models/Results.cs
@@ -0,0 +1,15 @@
+﻿namespace ASTquizApp.Models
+{
+    public class Result
+    {
+        public string CandidateName { get; set; }
+
+        public int TotalQuestions { get; set; }
+
+        public int CorrectAnswers { get; set; }
+
+        public int WrongAnswers { get; set; }
+
+        public double Percentage { get; set; }
+    }
+}
